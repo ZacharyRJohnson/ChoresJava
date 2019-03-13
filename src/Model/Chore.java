f20f8780598b813653bc0e236a01b38606d73d09
ZacharyRJohnson <zachjohnson1900@gmail.com>
2019-03-13 16:05:56 -0400

    Created gitignore and added owner field to Chore class

--- src/Model/Chore.java
+++ src/Model/Chore.java
@@ -1,17 +1,19 @@
 package Model;
 
 public class Chore {
-    private int due_date;   // Date the chore must be done. Format Month Day Year.
-                    // Example: May 10th 2018 = 05102018
+    private int due_date;   /* Date the chore must be done. Format Month Day Year.
+                             Example: May 10th 2018 = 05102018 */
     private int time;       // Time chore is to be done
     private String repeat;
     private String name;
+    private String owner;
 
-    public Chore(int date, int time, String repeat, String name) {
+    public Chore(int date, int time, String repeat, String name, String owner) {
         this.due_date = date;
         this.time = time;
         this.repeat = repeat;
         this.name = name;
+        this.owner = owner;
     }
 
     public int getDue_date() {
@@ -44,5 +46,13 @@ public class Chore {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getOwner(){
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 }
